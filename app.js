bf962059cf67e2f7b4117234908a39baf947e16c
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-01-06 15:12:50 -0800

    merge patrick and user branch

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'sample/build')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
