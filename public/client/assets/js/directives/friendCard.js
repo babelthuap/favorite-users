61cc48d3096a5e18b5cd02681b041f2d8b7d683b
Nicholas Neumann-Chun <nicholas.babelthuap (at) gmail.com>
2016-01-06 23:47:43 -0800

    Very rough draft of the friend card

---
+++ public/client/assets/js/directives/friendCard.js
@@ -0,0 +1,16 @@
+angular.module('application').directive('friendCard', friendCard);
+
+function friendCard() {
+  return {
+    restrict: "AE",
+    templateUrl: "templates/friendCard.html",
+    scope: {
+      user: "@"
+    },
+    controller: function($scope) {
+      'use strict';
+
+      $scope.data = JSON.parse($scope.user);
+    }
+  };
+}
