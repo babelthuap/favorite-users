2e7b7f650a5d64c6c307e57a11c5a3e123cd84ba
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-01-07 10:04:40 -0800

    newuser (signup) page & controller

--- public/client/assets/js/components/HomeCtrl.js
+++ public/client/assets/js/components/HomeCtrl.js
@@ -1,13 +1,19 @@
+(function() {
+  'use strict';
 
+  angular.module('application').controller('HomeCtrl', HomeCtrl);
 
-  angular.module('application')
-  .controller('HomeCtrl', HomeCtrl)
-  ;
   HomeCtrl.$inject = ['$scope', '$stateParams', '$state', '$controller'];
+
   function HomeCtrl($scope, $stateParams, $state, $controller) {
-    'use strict';
     angular.extend(this, $controller('DefaultController', {
-      $scope: $scope, $stateParams: $stateParams, $state: $state
+      $scope: $scope,
+      $stateParams: $stateParams,
+      $state: $state
     }));
+
     $scope.something = 'something cool';
+
+
   }
+})();
