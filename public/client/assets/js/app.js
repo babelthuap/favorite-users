1ae3501cc79e2414da68c8a1c75f359b380d0f01
Nicholas Neumann-Chun <nicholas.babelthuap (at) gmail.com>
2016-01-06 21:44:22 -0800

    Create rough draft of the user profile page

--- public/client/assets/js/app.js
+++ public/client/assets/js/app.js
@@ -12,6 +12,7 @@
   ])
     .config(config)
     .run(run)
+    .controller('ProfileCtrl', ProfileCtrl)
   ;
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
@@ -30,6 +31,56 @@
 
   function run() {
     FastClick.attach(document.body);
+  }
+
+
+  ProfileCtrl.$inject = ['$scope', '$stateParams', '$state', '$controller'];
+
+  function ProfileCtrl($scope, $stateParams, $state, $controller) {
+    angular.extend(this, $controller('DefaultController', {
+      $scope: $scope,
+      $stateParams: $stateParams,
+      $state: $state
+    }));
+    
+    // dummy data
+    $scope.user = {
+      name: "Bob",
+      email: "bob@bob.com",
+      phone: "555-555-5555",
+      address: "123 Drury Ln Somewhere, CA"
+    };
+    $scope.friends = [
+      {name: "Nicholas", email: "n@yahoo.com"},
+      {name: "Asami", email: "a@gmail.com"},
+      {name: "Zarathustra", email: "z@spake.co"},
+      {name: "Patrick", email: "p@gmail.com"}
+    ];
+
+    $scope.friends.sort(function(a, b) {
+      return a.name > b.name;
+    });
+
+    function escapeRegExp(string) {
+      return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    }
+
+    $scope.filterFriends = function(query) {
+      if (!query) {
+        return $scope.friends;
+      } else {
+        // filter where characters in the query string don’t have to be adjacent in the searched field(s)
+        let reRaw = '.*' + query.split('').map(escapeRegExp).join('.*') + '.*';
+        let re = new RegExp(reRaw, 'i');
+        return $scope.friends.filter(function(user) {
+          return ['name', 'email', 'phone', 'address'].some(function(field) {
+            return re.test(user[field]);
+          });
+        });
+      }
+    }
+
+
   }
 
 })();
