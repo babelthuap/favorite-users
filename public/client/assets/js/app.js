12b516831a4fb47104789d001389b8b7b0355f01
Asami Kawamoto <asami236@gmail.com>
2016-01-07 11:05:13 -0800

    Table is Responsive

--- public/client/assets/js/app.js
+++ public/client/assets/js/app.js
@@ -12,7 +12,6 @@
     ])
   .config(config)
   .run(run)
-  .controller('AdminCtrl', AdminCtrl)
   ;
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
@@ -33,30 +32,6 @@
     FastClick.attach(document.body);
   }
 
-  AdminCtrl.$inject = ['$scope', '$controller'];
 
-  function AdminCtrl($scope, $controller){
-    angular.extend(this, $controller('DefaultController', {
-      $scope: $scope,
-    }));
 
-    $scope.username = 'PAN';
-
-    $scope.users =  [
-    {
-      username: 'Kurokirishima',
-      email: 'kurokiri@gmail.com',
-      phone: '02564',
-      address: 'in the Cloud Blvd, Bahamas', 
-      avatar: 'this would be a picture'
-    },
-    {
-      username: 'Satsumabijin',
-      email: 'satsuma@gmail.com',
-      phone: '12989',
-      address: 'underwater', 
-      avatar: 'this would be a picture, too!'
-    }
-    ]  
-  };
 })();
