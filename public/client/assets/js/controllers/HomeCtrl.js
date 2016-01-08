bddc27a2da5e7fcc6fff310f72d068be9226f773
Missy Bur <missybur@gmail.com>
2016-01-07 21:54:06 -0800

    Store email to local storage and navigate to signup page

--- public/client/assets/js/controllers/HomeCtrl.js
+++ public/client/assets/js/controllers/HomeCtrl.js
@@ -3,17 +3,35 @@
 
   angular.module('application').controller('HomeCtrl', HomeCtrl);
 
-  HomeCtrl.$inject = ['$scope', '$stateParams', '$state', '$controller'];
+  HomeCtrl.$inject = ['$scope', '$stateParams', '$state', '$controller', '$localStorage'];
 
-  function HomeCtrl($scope, $stateParams, $state, $controller) {
+  function HomeCtrl($scope, $stateParams, $state, $controller, $localStorage) {
     angular.extend(this, $controller('DefaultController', {
       $scope: $scope,
       $stateParams: $stateParams,
-      $state: $state
+      $state: $state,
+      $localStorage: $localStorage
     }));
 
     $scope.welcome = 'Welcome To Friend Finder!';
 
+    $scope.$storage = $localStorage;
 
+    $scope.signup = function(email){
+      var email = $scope.email;
+      console.log(email);
+      $scope.$storage.email = email;
+      $state.go('signup');
+    };
   }
 })();
+
+// run(run);
+  // run.$inject = ['$rootScope'];
+  //   console.log('working')
+
+
+  // function run($rootScope) {
+  //   // $rootScope.$storage = $localStorage;
+  //   // $rootScope.$storage.$email = [];
+  // }
