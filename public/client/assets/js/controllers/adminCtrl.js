c3e0544bdfa1c293f9e924dd732b382548ca3441
Nicholas Neumann-Chun <nicholas.babelthuap (at) gmail.com>
2016-01-08 12:17:01 -0800

    Filter out admins

--- public/client/assets/js/controllers/adminCtrl.js
+++ public/client/assets/js/controllers/adminCtrl.js
@@ -1,37 +1,38 @@
-(function(){
-
+(function() {
   'use strict';
 
   angular.module('application').controller('AdminCtrl', AdminCtrl);
 
   AdminCtrl.$inject = ['$scope', '$controller', '$http', '$stateParams']
 
-  function AdminCtrl($scope, $controller, $http, $stateParams){
+  function AdminCtrl($scope, $controller, $http, $stateParams) {
     angular.extend(this, $controller('DefaultController', {
       $scope: $scope
     }));
 
-  let users = []; 
+    // dummy data
+    $scope.user = {
+      name: "PAMN"
+    };
 
-  const populateUsers = () => {
-    $http.get('/users').then((res) => {
-      console.log('res', res) 
-      $scope.users = res.data;
-    }).catch((err) => {
-      console.log('err', err)
-    })     
-  }
+    // populate users table
+    $http.get('/users')
+      .then(res => {
+        $scope.users = res.data.filter(user => !user.admin);
+        console.log($scope.users);
+      }).catch(err => {
+        console.log('err', err)
+      });
 
-  populateUsers ();  
-  $scope.username = 'PAMN';
-
-  $scope.deleteUser = (user) => {
-    let id = user._id
-    $http.delete(`/users/remove/${id}`).then((res) => {
-      populateUsers();
-    }).catch((err) => {
-      console.log('err', err)
-    })
-  }
+    $scope.deleteUser = function(user) {
+      let id = user._id;
+      $http.delete(`/users/remove/${id}`)
+        .then(res => {
+          populateUsers();
+        })
+        .catch(err => {
+          console.log('err', err)
+        });
+    }
   };
 })();
