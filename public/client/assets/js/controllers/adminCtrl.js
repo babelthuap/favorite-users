b1f45d47879f9179db0fc1ef356ccfee2c26d9c1
Asami Kawamoto <asami236@gmail.com>
2016-01-08 15:31:00 -0800

    Filter on Admin Page

--- public/client/assets/js/controllers/adminCtrl.js
+++ public/client/assets/js/controllers/adminCtrl.js
@@ -3,9 +3,9 @@
 
   angular.module('application').controller('AdminCtrl', AdminCtrl);
 
-  AdminCtrl.$inject = ['$scope', '$controller', '$http', '$stateParams']
+  AdminCtrl.$inject = ['$scope', '$controller', '$http', '$stateParams', 'UserSvc']
 
-  function AdminCtrl($scope, $controller, $http, $stateParams) {
+  function AdminCtrl($scope, $controller, $http, $stateParams, UserSvc) {
     angular.extend(this, $controller('DefaultController', {
       $scope: $scope
     }));
@@ -15,17 +15,39 @@
       name: "PAMNNN"
     };
 
+    $scope.allUsers = [];
+    
     function populateUsers() {
-      $http.get('/users')
-        .then(res => {
-          $scope.users = res.data.filter(user => !user.admin);
-          console.log($scope.users);
-        }).catch(err => {
-          console.log('err', err)
-        });
+      UserSvc.getAllUsers().then(resp => {
+        $scope.allUsers = resp.data;
+      })
     }
 
     populateUsers();
+
+    $scope.query = {};    
+    $scope.filterPeople = function() {
+      let filteredPeople = [].concat($scope.allUsers);
+      
+      ['name', 'email', 'address'].forEach(field => {
+        if ($scope.query[field]) {
+          let query = $scope.query[field].toLowerCase();
+          filteredPeople = filteredPeople.filter(user => {
+            return user[field].toLowerCase().indexOf(query) !== -1;
+          });
+        }
+      });
+
+      //Made a separate function for phones to enable search with or without "()" or "-"
+      if ($scope.query.phone) {
+        let query = $scope.query.phone.replace(/\D/g, "");
+        filteredPeople = filteredPeople.filter(user => {
+          return user.phone.replace(/\D/g, "").indexOf(query) !== -1;
+        });
+      }
+      
+      return filteredPeople;
+    }
 
     $scope.deleteUser = function(user) {
       let id = user._id;
