4b72d7378a3cd4d59f2cb37e83aa20aa8ab29135
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-01-08 12:17:54 -0800

    add profile pic to dummy data

--- scripts/hashpasswords.js
+++ scripts/hashpasswords.js
@@ -2,12 +2,15 @@ var data = require('../data/users.json');
 var fs = require('fs');
 var bcrypt = require('bcryptjs')
 var CONFIG = require('../util/authConfig');
+var path = require('path');
 
 data = data.map(function(doc){
 
   var salt = bcrypt.genSaltSync(CONFIG.saltRounds);
   var hash = bcrypt.hashSync(doc.password, salt);
   doc.password = hash;
+  doc.profilePic = "http://placehold.it/350x300";
+  console.log(doc);
   return doc;
 
 });
